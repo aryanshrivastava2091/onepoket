a077d67766a8b280acc3f4a7f09acfaa12c57c22
aryan <89937885+aryanshrivastava2091@users.noreply.github.com>
2023-07-24 12:50:49 +0530

    Create App.jsx for Task 3
    Task 3 App.jsx file

---
+++ Task3/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const DataFetchingComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.example.com/data');
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Data List</h2>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <strong>{item.name}</strong>: {item.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DataFetchingComponent;
